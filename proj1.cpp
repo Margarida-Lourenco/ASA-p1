aa56d811d065875c1b1be64b0fe0ecc81ab3480b
Margarida Ascensão Lourenço <margarida.a.lourenco@tecnico.ulisboa.pt>
2023-11-15 23:30:44 +0000

    just inputs

---
+++ proj1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int X,Y;
+    int n;
+    cin >> X >> Y;  //chapa dimensions
+    cin >> n;       // types os chapas
+
+    for(int i = 0; i<n; i++){
+        int a,b, price;
+        cin >> a >> b >> price;
+    }
+
+}
