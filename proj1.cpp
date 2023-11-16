c46c96fa3e787ecdde988059b0c6df699e959a94
Inês Paredes <ines.paredes@tecnico.ulisboa.pt>
2023-11-16 10:53:43 +0000

    código base que funciona para exemplos

--- proj1.cpp
+++ proj1.cpp
@@ -2,14 +2,28 @@
 using namespace std;
 
 int main(){
-    int X,Y;
-    int n;
+    int X, Y;
+    int n, areaChapa, result = 0;
     cin >> X >> Y;  //chapa dimensions
     cin >> n;       // types os chapas
+    areaChapa = X * Y;
 
-    for(int i = 0; i<n; i++){
-        int a,b, price;
+    for(int i = 1; i <= n; i++){
+        int a, b, price;
         cin >> a >> b >> price;
-    }
+        if (a > X || b > Y){
+            cout << "0" << endl;
+            break;
+        }
+        int areaPeca = a * b;
+        int qtd = areaChapa / areaPeca;
+        int totalPrice = qtd * price;
 
+        if (totalPrice > result){
+            result = totalPrice;
+        }
+    }
+    
+    cout << result << endl;
+    return 0;
 }
