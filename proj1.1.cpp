269c11eeb3300a8b285242f20758ad8532dd71fe
Margarida Ascensão Lourenço <margarida.a.lourenco@tecnico.ulisboa.pt>
2023-11-27 20:16:17 +0000

    versão toppp

--- proj1.1.cpp
+++ proj1.1.cpp
@@ -1,44 +1,46 @@
 #define max(a, b) (a > b ? a : b)
 
-#include <iostream>
+#include <stdio.h>
 #include <vector>
 using namespace std;
 
-// Recursive function that realizes the cuts
 int main(){
     int X, Y, n;
 
-    cin >> X >> Y;  // Plate dimensions
-    cin >> n;       // Types of pieces
+    if (scanf("%d %d\n%d", &X , &Y, &n) != 3)   // Plate dimensions and number of pieces
+        return 1;                               
 
-    vector<vector<int>> dp(X + 1, vector<int>(Y + 1, 0));
+    vector<int> dp((X + 1)*(Y + 1), 0);
 
     for(int i = 1; i <= n; i++){
         int a, b, price;
-        cin >> a >> b >> price;
+
+        if(scanf("%d %d %d", &a , &b, &price) != 3)  // Piece dimensions and price
+            return 1;  
 
         if (a <= X && b <= Y) {
-            dp[a][b] = max(dp[a][b], price);
+            dp[a + X * b] = max(dp[a + X * b], price);
         }
+        
         // Rotate 
         if (a <= Y && b <= X) {
-            dp[b][a] = max(dp[b][a], price);
+            dp[b + X * a] = max(dp[b + X * a], price);
         }
     }
 
     for (int i = 1; i <= X; i++) {
         for (int j = 1; j <= Y; j++) {
-            for (int k = 1; k <= i; k++) {
+            for (int k = 1; k <= i/2; k++) {
                 // Cut vertically
-                dp[i][j] = max(dp[i][j], dp[k][j] + dp[i - k][j]);
+                dp[i + X * j] = max(dp[i + X * j], dp[k + X * j] + dp[(i - k) + X * j]);
             }
-            for (int k = 1; k <= j; k++) {
+            for (int k = 1; k <= j/2; k++) {
                 // Cut horizontally
-                dp[i][j] = max(dp[i][j], dp[i][k] + dp[i][j - k]);
+                dp[i + X * j] = max(dp[i + X * j], dp[i + X * k] + dp[i + X * (j - k)]);
             }
         }
     }
 
-    cout << dp[X][Y] << endl;
+    printf("%d\n", dp[X + X * Y]);
     return 0;
 }
